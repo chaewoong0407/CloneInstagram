692648f3d864849bfa67555eab520d089dbf6fbd
chaewoong0407 <an0304073@naver.com>
2021-09-08 11:57:09 +0900

    [INIT] Creact React-native App

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>안녕리알!</Text>
+    </View>
+  );
+};
+
+export default App;
